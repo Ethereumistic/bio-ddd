ac5724c5d24858d237fe546fd4784130d81585fb
Ethereumistic <badjarovv@gmail.com>
2026-02-11 12:45:29 +0200

    updatez2

--- components/NavbarDemo.tsx
+++ components/NavbarDemo.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
-import { HoveredLink, LogoImage,  Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { HoveredLink, LogoImage, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import ThemeSwitch from "./themeSwitch";
 import Image from "next/image";
@@ -63,32 +63,32 @@ export function NavbarDemo({ className }: { className?: string }) {
   return (
     <div className="relative w-full flex items-center justify-center">
       <AnimatePresence mode="wait">
-      <motion.div
-        initial={{
-          opacity: 1,
-          y: -100,
-        }}
-        animate={{
-          y: visible ? 0 : -100,
-          opacity: visible ? 1 : 0,
-        }}
-        transition={{
-          duration: 0.2,
-        }}
+        <motion.div
+          initial={{
+            opacity: 1,
+            y: -100,
+          }}
+          animate={{
+            y: visible ? 0 : -100,
+            opacity: visible ? 1 : 0,
+          }}
+          transition={{
+            duration: 0.2,
+          }}
           className={cn(
             "flex fixed top-0 inset-x-0 mx-auto rounded-full bg-transparent z-[5000] items-center justify-center",
             className
           )}
         >
-          <Navbar 
-            className="top-0" 
-            isMobile={isMobile} 
+          <Navbar
+            className="top-0"
+            isMobile={isMobile}
             mobileMenuOpen={mobileMenuOpen}
-            setMobileMenuOpen={setMobileMenuOpen} 
+            setMobileMenuOpen={setMobileMenuOpen}
           />
         </motion.div>
       </AnimatePresence>
-      
+
       {isMobile && (
         <div ref={menuRef}>
           <MobileMenu isOpen={mobileMenuOpen} onClose={() => setMobileMenuOpen(false)} />
@@ -98,17 +98,18 @@ export function NavbarDemo({ className }: { className?: string }) {
   );
 }
 
-function Navbar({ 
-  className, 
-  isMobile, 
+function Navbar({
+  className,
+  isMobile,
   mobileMenuOpen,
-  setMobileMenuOpen 
-}: { 
-  className?: string, 
-  isMobile: boolean, 
+  setMobileMenuOpen
+}: {
+  className?: string,
+  isMobile: boolean,
   mobileMenuOpen: boolean,
-  setMobileMenuOpen: (isOpen: boolean) => void 
-}) {  const [active, setActive] = useState<string | null>(null);
+  setMobileMenuOpen: (isOpen: boolean) => void
+}) {
+  const [active, setActive] = useState<string | null>(null);
   return (
     <div className="w-full px-4 sm:px-6 lg:px-8">
       <div className={cn("max-w-7xl mx-auto", className)}>
@@ -128,31 +129,32 @@ function Navbar({
             {isMobile ? (
               <div className="flex items-center space-x-4">
                 <div className="" >
-                  <ThemeSwitch  />
+                  <ThemeSwitch />
                 </div>
-              <button 
-                onClick={() => setMobileMenuOpen(!mobileMenuOpen)} 
-                className="text-3xl z-[5001] ml-4"
-              >
-                {mobileMenuOpen ? <HiX /> : <HiMenu />}
-              </button>
+                <button
+                  onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+                  className="text-3xl z-[5001] ml-4"
+                >
+                  {mobileMenuOpen ? <HiX /> : <HiMenu />}
+                </button>
               </div>
-              
+
             ) : (
               <div className="flex items-center font-russo space-x-2 xs:space-x-3 sm:space-x-4 md:space-x-6 lg:space-x-12">
                 <Link href="/pests" className="text-lg ">
-                <div className="flex justify-center text-center">
-                  <HoverBorderGradient
-                    containerClassName="rounded-full"
-                    as="button"
-                    className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-1 sm:space-x-2 
+                  <div className="flex justify-center text-center">
+                    <HoverBorderGradient
+                      containerClassName="rounded-full"
+                      as="button"
+                      className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-1 sm:space-x-2 
                                px-2 py-1 sm:px-3 sm:py-2 md:px-4 md:py-2 lg:px-5 lg:py-3
                                text-sm sm:text-sm md:text-base lg:text-lg transition-all duration-300"
-                  >
-                  <MenuItem setActive={setActive} active={active} item="Услуги">
-                  
-                    <div className="flex justify-center items-center my-4"><IconSpray width={50} height={50} /><FlipWordsNav /></div>
-                    <div className="max-h-[calc(100vh-200px)] overflow-y-auto custom-scrollbar">
+                    >
+                      <span>Услуги</span>
+                      {/* <MenuItem setActive={setActive} active={active} item="Услуги"> */}
+
+                      {/* <div className="flex justify-center items-center my-4"><IconSpray width={50} height={50} /><FlipWordsNav /></div> */}
+                      {/* <div className="max-h-[calc(100vh-200px)] overflow-y-auto custom-scrollbar">
 
                     <div className="text-sm grid gap-6 p-4
                       min-[932px]:grid-cols-2 
@@ -288,45 +290,45 @@ function Navbar({
               description="Престанете да страдаете от дървоядите!"
             />
                       </div>
-                      </div>
-                  </MenuItem>
-                  
-                  </HoverBorderGradient>
+                      </div> */}
+                      {/* </MenuItem> */}
+
+                    </HoverBorderGradient>
                   </div>
                 </Link>
 
                 <Link href="/business" className="text-lg">
-                <div className="flex justify-center text-center">
-                <HoverBorderGradient
-                  containerClassName="rounded-full"
-                  as="button"
-                  className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-1 sm:space-x-2 
+                  <div className="flex justify-center text-center">
+                    <HoverBorderGradient
+                      containerClassName="rounded-full"
+                      as="button"
+                      className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-1 sm:space-x-2 
                              px-2 py-1 sm:px-3 sm:py-2 md:px-4 md:py-2 lg:px-5 lg:py-3
                              text-sm sm:text-sm md:text-base lg:text-lg transition-all duration-300"
-                >
-                  <MenuItem setActive={setActive} active={active} item="Бизнес">
-                    <div className="flex flex-col text-lg px-8 my-10">
-                      <div className="flex flex-row  pb-10  justify-center hover:scale-105 transition duration-300 hover:drop-shadow-[0_1.5px_1.5px_rgba(94,187,70,1)]">
-                      <IconTie width={30} height={30} className="mr-2" />
-                      <HoveredLink href="/business">Бизнес Услуги</HoveredLink>
-                      </div>
-                      <hr></hr>
-                      <div className="flex flex-row pt-10  hover:scale-105 transition duration-300 hover:drop-shadow-[0_1.5px_1.5px_rgba(94,187,70,1)]">
-                      <IconContract width={30} height={30} className="mr-2" />
-                      <HoveredLink href="/business/contract">ДДД Договор</HoveredLink>
-                      </div>
+                    >
+                      <MenuItem setActive={setActive} active={active} item="Бизнес">
+                        <div className="flex flex-col text-lg px-8 my-10">
+                          <div className="flex flex-row  pb-10  justify-center hover:scale-105 transition duration-300 hover:drop-shadow-[0_1.5px_1.5px_rgba(94,187,70,1)]">
+                            <IconTie width={30} height={30} className="mr-2" />
+                            <HoveredLink href="/business">Бизнес Услуги</HoveredLink>
+                          </div>
+                          <hr></hr>
+                          <div className="flex flex-row pt-10  hover:scale-105 transition duration-300 hover:drop-shadow-[0_1.5px_1.5px_rgba(94,187,70,1)]">
+                            <IconContract width={30} height={30} className="mr-2" />
+                            <HoveredLink href="/business/contract">ДДД Договор</HoveredLink>
+                          </div>
 
-                      {/* <div className=" hover:scale-105 transition duration-300 hover:drop-shadow-[0_1.5px_1.5px_rgba(255,22,22,1)]">
+                          {/* <div className=" hover:scale-105 transition duration-300 hover:drop-shadow-[0_1.5px_1.5px_rgba(255,22,22,1)]">
                       <HoveredLink href="/seo">Контрол на Гризачи</HoveredLink>
                       </div>
 
                       <div className=" hover:scale-105 transition duration-300 hover:drop-shadow-[0_1.5px_1.5px_rgba(255,22,22,1)]">
                       <HoveredLink href="/branding">Контрол на Мравки</HoveredLink>
                       </div> */}
-                    </div>
-                  </MenuItem>
-                </HoverBorderGradient>
-                </div>
+                        </div>
+                      </MenuItem>
+                    </HoverBorderGradient>
+                  </div>
                 </Link>
 
 
